8c14fcf6a7414a24ca73666ba99d6a863816b210
NigoDaS <nigothesolo@gmail.com>
2025-07-12 13:34:26 +0200

    test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const pi = Math.PI;
+let radius;
+let circumference;
+
+radius = window.prompt('Enter the radius of a circle');
+radius = Number(radius);
+
+circumference = 2 * pi * radius
+console.log(circumference)
