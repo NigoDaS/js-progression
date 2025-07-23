eeaa26b1b6427ef53acaf63c02fe7cf37b1aa596
NigoDaS <nigothesolo@gmail.com>
2025-07-23 14:35:15 +0200

    cartQuantity counter added

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -79,6 +79,12 @@ document.querySelectorAll('.js-add-to-cart')
         })
       }
 
-      console.log(cart)
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      })
+
+      document.querySelector('.cart-quantity').innerHTML = cartQuantity;
     });
   });
