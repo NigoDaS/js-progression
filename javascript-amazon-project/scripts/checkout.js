32d87790b609bed9ba026f7627ba404e7c9ae060
NigoDaS <nigothesolo@gmail.com>
2025-07-28 11:14:38 +0200

    added functional cart

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,7 +1,7 @@
 import {cart} from '../data/cart.js'
 import {products} from '../data/products.js';
 
-let cartHTML = '';
+let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
 
 
 
-  `
+  cartSummaryHTML += `
   <div class="cart-item-container">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
@@ -26,18 +26,18 @@ cart.forEach((cartItem) => {
 
     <div class="cart-item-details-grid">
       <img class="product-image"
-        src="">
+        src="${matchingProduct.image}">
 
       <div class="cart-item-details">
         <div class="product-name">
-          Black and Gray Athletic Cotton Socks - 6 Pairs
+          ${matchingProduct.name}
         </div>
         <div class="product-price">
-          $10.90
+          ${matchingProduct.priceCents / 100}
         </div>
         <div class="product-quantity">
           <span>
-            Quantity: <span class="quantity-label">2</span>
+            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
           </span>
           <span class="update-quantity-link link-primary">
             Update
@@ -94,5 +94,7 @@ cart.forEach((cartItem) => {
       </div>
     </div>
   </div>
-  `
+  `;
 });
+
+document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
